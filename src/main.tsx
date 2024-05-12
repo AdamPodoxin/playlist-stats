23ee349680645424d8a3e11d6dfdc37866822729
Adam Podoxin <adam.podoxin@gmail.com>
2024-05-12 13:49:09 -0700

    Setup router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
+import CallbackPage from "./pages/Callback";
+import PlaylistStatsPage, {
+	loader as playlistLoader,
+} from "./pages/PlaylistStats";
+
 import "./index.css";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+	{
+		path: "/callback",
+		element: <CallbackPage />,
+	},
+	{
+		path: "/playlist/:playlistId",
+		element: <PlaylistStatsPage />,
+		loader: playlistLoader,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<main className="flex min-h-screen flex-col items-center p-8 gap-4">
-			<App />
+			<RouterProvider router={router} />
 		</main>
 	</React.StrictMode>
 );
