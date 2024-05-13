8c31cc51f4c7401bd0524a4fad962caeffac6b44
Adam Podoxin <adam.podoxin@gmail.com>
2024-05-12 19:07:47 -0700

    Implement code challenge

---
+++ src/utils/codeChallenge.ts
@@ -0,0 +1,33 @@
+const generateRandomString = (length: number) => {
+	const possible =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	const values = crypto.getRandomValues(new Uint8Array(length));
+
+	return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+};
+
+const sha256 = async (plain: string) => {
+	const encoder = new TextEncoder();
+	const data = encoder.encode(plain);
+
+	return await window.crypto.subtle.digest("SHA-256", data);
+};
+
+const base64encode = (input: ArrayBuffer) => {
+	const textDecoder = new TextDecoder();
+	const decodedInput = textDecoder.decode(input);
+
+	return btoa(decodedInput)
+		.replace(/=/g, "")
+		.replace(/\+/g, "-")
+		.replace(/\//g, "_");
+};
+
+export const generateCodeVerifier = () => generateRandomString(64);
+
+export const generateCodeChallenge = async (codeVerifier: string) => {
+	const hashed = await sha256(codeVerifier);
+	const codeChallenge = base64encode(hashed);
+
+	return codeChallenge;
+};
